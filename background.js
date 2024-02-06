fb0b64d44ebf6820d35e8431a9a527d90354fc6f
Mango <evan940415@gmail.com>
2024-02-07 02:02:41 +0800

    prettier

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 chrome.runtime.onMessage.addListener((request) => {
   if (request.action === "refreshBlockedDomains") {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       if (tabs.length > 0) {
         chrome.tabs.sendMessage(tabs[0].id, request);
       }
